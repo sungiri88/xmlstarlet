822f3bab3f4461312fa1c015996055895032efae
Umapathy <sungiri88@gmail.com>
2019-05-08 12:00:42 +0530

    Chocolatey package for xmlstarlet

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿try {
+  $package = 'xmlstarlet'
+  $PackageVersion = "1.6.1"
+  $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  $filename       = 'xmlstarlet-1.6.1-win32.zip'
+  $url          = "$toolsDir\$filename"
+  $destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+  Install-ChocolateyZipPackage $package -url $url -unzipLocation $destination
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
